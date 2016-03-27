1335653ba1cef9a7b22aae18d7ae761e0544b297
Benedikt Wedenik <benedikt.wedenik@gmail.com>
2016-03-27 10:38:10 +0200

    assignment 2 part 4

--- assg2/Assg2.hs
+++ assg2/Assg2.hs
@@ -13,13 +13,43 @@ decimals n = decimals (n `div ` 10) ++ [n `mod` 10]
 
 
 -- Ex 4
+--  Goedel Zahlen
 gz :: Integer -> Integer
 gz n
 	    | n <= 0 = 0
-	    | otherwise = foldl (*) 1 ( zipWith (**) primes ( decimals n ) )
+	    | otherwise = foldl (*) 1 ( zipWith (^) primes ( decimals n ) )
 
+-- Stream of goedle numbers
 gzs :: [Integer]
 gzs = map gz [1..]
---------------------------------------------
 
+-- goedel numbers with memoization
+gzMT :: Integer -> Integer
+gzMT n
+	    | n <= 0 = 0
+	    | otherwise = helpGz (decimals n) 0
+
+-- recursive function to combine (multiply) results from memo table
+helpGz :: [Integer] -> Integer -> Integer
+helpGz [] _ = 1
+helpGz (x:xs) i = ( (gzMTList !! fromIntegral(x)) !! fromIntegral(i) ) * ( helpGz xs (i+1))
+
+-- memo list of primes
+gzMTList = [
+            [1,1..],
+            primes,
+            map (^2) primes,
+            map (^3) primes,
+            map (^4) primes,
+            map (^5) primes,
+            map (^6) primes,
+            map (^7) primes,
+            map (^8) primes,
+            map (^9) primes
+           ]
+
+-- stream of goedel numbers with memoization
+gzsMT :: [Integer]
+gzsMT = map gzMT [1..]
+--------------------------------------------
 
