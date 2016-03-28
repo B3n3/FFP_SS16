92e152f2738ef4ee3fd7f9c3858985a865fa984b
Mihai Lepadat <mihailepadat@gmail.com>
2016-03-28 18:10:54 +0200

    assignment2: ex 1, 2, 3

--- assg2/Assg2.hs
+++ assg2/Assg2.hs
@@ -9,6 +9,50 @@ sieve (x:xs) = x : sieve [y | y <- xs, mod y x > 0]
 -- Get the decimals of a number in list form
 decimals 0 = []
 decimals n = decimals (n `div ` 10) ++ [n `mod` 10]
+
+-- Check if a number is in a _sorted_ list
+isIn :: Integer -> [Integer] -> Bool
+isIn n (x:xs)
+    | n > x     = isIn n xs
+    | n == x    = True
+    | otherwise = False
+--------------------------------------------
+
+-- Ex 1
+nOfOnesInBinary :: Integer -> Integer
+nOfOnesInBinary n
+    | n == 0    = 0
+    | otherwise = (n `mod` 2) + nOfOnesInBinary(n `div` 2)
+
+isPrime :: Integer -> Bool
+isPrime n = n `isIn` primes
+
+ddps :: [Integer]
+ddps = [x | x <- [2..], isPrime(nOfOnesInBinary(x))]
+--------------------------------------------
+
+
+-- Ex 2
+isPrimeList :: [Bool]
+isPrimeList = [isPrime x | x <- [0..]]
+
+ddpsMT :: [Integer]
+ddpsMT = [x | x <- [2..], isPrimeList !! fromIntegral(nOfOnesInBinary(x))]
+--------------------------------------------
+
+
+-- Ex 3
+pow3List :: [Integer]
+pow3List = [powMT x | x <- [0..]]
+
+powMT :: Int -> Integer
+powMT 0 = 1
+powMT n = (pow3List !! (n-1)) + (pow3List !! (n-1)) + (pow3List !! (n-1))
+
+-- naive implemtantion (bad and also without memoization)
+--pow :: Int -> Integer
+--pow 0 = 1
+--pow n = pow(n-1) + pow(n-1) + pow(n-1)
 --------------------------------------------
 
 
--- assg2/Assg2Test.hs
+++ assg2/Assg2Test.hs
@@ -41,5 +41,39 @@ test9 = TestCase $ assertEqual "Goedle number stream (gzsMT) wrong"
             [2,4,8,16,32,64,128,256,512,2,6,18,54,162,486,1458,4374,13122,39366,4]
             (take 20 gzsMT)
 
+test10 = TestCase $ assertEqual "nOfOnesInBinary"
+            2
+            (nOfOnesInBinary 3)
+
+test11 = TestCase $ assertEqual "isPrime - it is prime"
+            True
+            (isPrime 541)
+
+test12 = TestCase $ assertEqual "isPrime - it is not prime"
+            False
+            (isPrime 542)
+
+test13 = TestCase $ assertEqual "ddps"
+            [3,5,6,7,9,10,11,12,13,14]
+            (take 10 ddps)
+
+test14 = TestCase $ assertEqual "ddps"
+            31
+            (ddps!!20)
+
+test15 = TestCase $ assertEqual "ddpsMT"
+            31
+            (ddpsMT!!20)
+
+test16 = TestCase $ assertEqual "ddpsMT"
+            31
+            (ddpsMT!!20)
+
+test17 = TestCase $ assertEqual "3 ^ 10 should be 59049"
+            59049
+            (powMT 10)
+
+
 main :: IO Counts
-main = runTestTT $ TestList [test1, test2, test3, test4, test5, test6, test7, test8, test9]
+main = runTestTT $ TestList [test1, test2, test3, test4, test5, test6, test7, test8, test9,
+            test10, test11, test12, test13, test14, test15, test16, test17]
