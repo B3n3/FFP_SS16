7daf57ff06324831aa474e06bcd474ffa3f32016
Valentin - Mihai Neacsu <neacsustoi_92@yahoo.com>
2016-04-07 22:09:26 +0200

    Ass 3, Ue 1 - spgGenerator implemented

--- assg3/Assg3.hs
+++ assg3/Assg3.hs
@@ -24,14 +24,24 @@ springer s a z = spgTransformer . spgSelektor . spgFilter . spgGenerator . trans
 
 
 transform :: (StartPosition, AnzahlZuege, ZielPosition) -> (VirtualVonPosition, AnzahlZuege, VirtualNachPosition)
-transform ((sr, sl), a, (zr, zl)) = 
-	let convert e = fromIntegral $ fromEnum e
+transform ((sr, sl), a, (zr, zl)) =
+	let convert e = fromIntegral $ (fromEnum e) + 1
 	in ((convert sr, convert sl), a, (convert zr, convert zl))
 
-spgGenerator :: (VirtualVonPosition, AnzahlZuege, VirtualNachPosition) -> [VirtualZugfolge]
-spgGenerator _ = []
+spgGenerator :: (VirtualVonPosition, AnzahlZuege, VirtualNachPosition) -> (VirtualNachPosition, [VirtualZugfolge])
+spgGenerator (_, 0, np) = (np, [[]])
+spgGenerator (vp@(vpr, vpl), a, np) = (np, moves1 ++ moves2 ++ moves3 ++ moves4 ++ moves5 ++ moves6 ++ moves7 ++ moves8)
+	where
+		moves1 = map ((vp, (vpr - 2, vpl + 1)):) $ snd $ spgGenerator ((vpr - 2, vpl + 1), a - 1, np)
+		moves2 = map ((vp, (vpr - 1, vpl + 2)):) $ snd $ spgGenerator ((vpr - 1, vpl + 2), a - 1, np)
+		moves3 = map ((vp, (vpr + 1, vpl + 2)):) $ snd $ spgGenerator ((vpr + 1, vpl + 2), a - 1, np)
+		moves4 = map ((vp, (vpr + 2, vpl + 1)):) $ snd $ spgGenerator ((vpr + 2, vpl + 1), a - 1, np)
+		moves5 = map ((vp, (vpr + 2, vpl - 1)):) $ snd $ spgGenerator ((vpr + 2, vpl - 1), a - 1, np)
+		moves6 = map ((vp, (vpr + 1, vpl - 2)):) $ snd $ spgGenerator ((vpr + 1, vpl - 2), a - 1, np)
+		moves7 = map ((vp, (vpr - 1, vpl - 2)):) $ snd $ spgGenerator ((vpr - 1, vpl - 2), a - 1, np)
+		moves8 = map ((vp, (vpr - 2, vpl - 1)):) $ snd $ spgGenerator ((vpr - 2, vpl - 1), a - 1, np)
 
-spgFilter :: [VirtualZugfolge] -> [VirtualZugfolge]
+spgFilter :: (VirtualNachPosition, [VirtualZugfolge]) -> [VirtualZugfolge]
 spgFilter _ = []
 
 spgSelektor :: [VirtualZugfolge] -> [VirtualZugfolge]
