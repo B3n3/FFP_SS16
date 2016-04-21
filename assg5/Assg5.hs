b86308a4939d9c56188e2c2334dbcb1bc6233c0e
Benedikt Wedenik <benedikt.wedenik@gmail.com>
2016-04-21 09:59:35 +0200

    ex1 and ex2 of assignment 5

---
+++ assg5/Assg5.hs
@@ -0,0 +1,35 @@
+module Assg5 where
+
+import Array
+
+-- Ex 1
+
+mas :: Array Int Int -> Int
+mas a = maximum ( map snd ( sumsOfSection a ) )
+
+
+-- helper to get all possible sections
+sections :: Ix a => Array a b -> [(a,a)]
+sections a = [(x,y) | x <- (indices a), y <- (indices a), x<=y]
+
+-- helper to calculate the sum of one section
+sectionSum :: (Ix a, Num b) => Array a b -> (a,a) -> b
+sectionSum a section = sum ( map (a !) ( range section ) )
+
+-- helper to get a list of pairs of   section-range + the sum of it
+sumsOfSection :: (Ix a, Num b) => Array a b -> [((a,a),b)]
+sumsOfSection a = map (\index -> (index,sectionSum a index)) $ sections a
+
+-----------------------------
+
+
+-- Ex 2
+
+amas :: Array Int Int -> [(Int,Int)]
+-- compare each sum with the result of mas, with filter only the matching
+-- elements get selected (fst takes the first element which is the index)
+amas a = map fst ( filter (\(_,max) -> max == mas a) (sumsOfSection a)  )
+
+-----------------------------
+
+
---
+++ assg5/Assg5Test.hs
@@ -0,0 +1,26 @@
+module Assg5Test where
+
+import Test.HUnit
+import Data.Monoid
+import Control.Monad
+import Assg5
+
+
+main :: IO Counts
+main = runTestTT $ TestList [ ]
+
+a = array (1,9) [(1,3),(2,(-5)),(3,0),(4,9),(5,2),(6,(-1)),(7,2),(8,(-5)),(9,1)]
+b = array (1,9) [(1,3),(2,(-1)),(3,(-2)),(4,9),(5,2),(6,(-1)),(7,2),(8,0),(9,(-1))]
+
+masTest1 = TestCase $ assertEqual "mas"
+                12
+                $ mas a
+
+amasTest1 = TestCase $ assertEqual "amas"
+                [(3,7),(4,7)]
+                $ amas a
+
+amasTest2 = TestCase $ assertEqual "amas"
+                [(1,7),(1,8),(4,7),(4,8)]
+                $ amas b
+
