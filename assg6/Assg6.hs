bd9f61b5c6b68a8c36c9660ae16d8833130bed2d
Valentin - Mihai Neacsu <neacsustoi_92@yahoo.com>
2016-05-03 19:59:14 +0200

    ass6 - 1, 2

--- assg6/Assg6.hs
+++ assg6/Assg6.hs
@@ -1,9 +1,39 @@
 module Assg6 where
 
+data Token = Wrong | Mult | Add | Value Integer deriving (Eq,Show)
+
 --Ex 1
 
+inOut :: (String -> String) -> IO Integer
+inOut f = do
+    lines <- readUntilEnd
+    sequence $ map (print . f) lines
+    return $ fromIntegral $ length lines
+
+readUntilEnd :: IO [String]
+readUntilEnd = do
+    line <- getLine
+    if line == "end"
+        then return []
+        else do
+            lines <- readUntilEnd
+            return $ line:lines
 
 --Ex 2
+
+tokenize :: String -> [Token]
+tokenize "" = []
+tokenize s@(c:r)
+    | elem c whitespaceChars = tokenize r 
+    | c == '*' = Mult : tokenize r
+    | c == '+' = Add : tokenize r
+    | elem c valueChars = (parseValue s) : (tokenize $ nextAfterValue s)
+    | otherwise = Wrong : tokenize r
+    where
+        whitespaceChars = " \t"
+        valueChars = "-0123456789"
+        parseValue s = Value $ read $ takeWhile (\c -> elem c valueChars) s
+        nextAfterValue s = dropWhile (\c -> elem c valueChars) s
 
 
 --Ex 3
--- assg6/Assg6Test.hs
+++ assg6/Assg6Test.hs
@@ -5,4 +5,8 @@ import Assg6
 
 
 main :: IO Counts
-main = runTestTT $ TestList []
+main = runTestTT $ TestList [testTokenize]
+
+testTokenize = TestCase $ assertEqual "tokenize"
+                [Value 123, Add, Value (-123), Wrong, Wrong, Mult, Value (-1)]
+                $ tokenize "123+-123 wr * -01"
