4e84e492e565b3e51f2a9bd412068f1f3eaa5b2e
Benedikt Wedenik <benedikt.wedenik@gmail.com>
2016-05-11 00:51:58 +0200

    fix in ass7, ex2 not working

--- assg7/Assg7.hs
+++ assg7/Assg7.hs
@@ -12,8 +12,21 @@ occS text word = occS' (length word - 1) (length word - 1) word text
 occS' :: Last -> First -> Assg7.Word -> Text -> [(First, Last)]
 occS' last first word text
     | last >= length text   = []
-    | last - first + 1 == length word
-        = (first, last) : occS' (last + length word) (last + length word) word text
+    | last - first == length word
+        = (first+1, last) : occS' (last + length word) (last + length word) word text
     | text !! first == word !! (length word - (last - first + 1))
         = occS' last (first - 1) word text
     | otherwise             = occS' (last + 1) (last + 1) word text
+
+
+occI :: Text -> Assg7.Word -> [(First, Last)]
+occI text word = occI' (length word - 1) (length word - 1) word text
+
+occI' :: Last -> First -> Assg7.Word -> Text -> [(First, Last)]
+occI' last first word text
+    | last >= length text   = []
+    | last - first == length word
+        = (first+1, last) : occI' (last + length word) (last + length word) word text
+    | text !! first  == word !! (length word - (last - first + 1))
+        = occI' last (first-1) word text
+    | otherwise = []
