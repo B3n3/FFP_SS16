f598585176e72f1753fc5036d995770bfb517f43
Valentin - Mihai Neacsu <neacsustoi_92@yahoo.com>
2016-05-26 17:24:33 +0200

    simpleMSS

--- assg8/Assg8.hs
+++ assg8/Assg8.hs
@@ -2,4 +2,11 @@ module Assg8 where
 
 
 simpleMSS :: [Int] -> [Int]
-simpleMSS _ = []
+simpleMSS = snd . maximum . zipSum . segments
+
+segments :: [Int] -> [[Int]]
+segments [] = []
+segments l = l : ((segments $ init l) ++ (segments $ tail l))
+
+zipSum :: [[Int]] -> [(Int, [Int])]
+zipSum l = zip (map sum l) l 
--- assg8/Assg8Test.hs
+++ assg8/Assg8Test.hs
@@ -4,8 +4,12 @@ import Test.HUnit
 import Assg8
 
 main :: IO Counts
-main = runTestTT $ TestList [testSimpleMSS]
+main = runTestTT $ TestList [testSimpleMSS1, testSimpleMSS2]
 
-testSimpleMSS = TestCase $ assertEqual "simpleMSS"
+testSimpleMSS1 = TestCase $ assertEqual "simpleMSS"
                 [2,1]
                 $ simpleMSS [-4,-3,-7,2,1,-2,-1,-4]
+
+testSimpleMSS2 = TestCase $ assertEqual "simpleMSS"
+                [7,1,-2,-1,-4,10]
+                $ simpleMSS [-4,-3,-7,7,1,-2,-1,-4,10]
