e2378cd8eea4b4b7a84c0625b841af07424d7317
Valentin - Mihai Neacsu <neacsustoi_92@yahoo.com>
2016-05-26 19:20:23 +0200

    smartMSS

--- assg8/Assg8.hs
+++ assg8/Assg8.hs
@@ -9,4 +9,13 @@ segments [] = []
 segments l = l : ((segments $ init l) ++ (segments $ tail l))
 
 zipSum :: [[Int]] -> [(Int, [Int])]
-zipSum l = zip (map sum l) l 
+zipSum l = zip (map sum l) l
+
+smartMSS :: [Int] -> [Int]
+smartMSS = fourth . foldr step (0,0,[],[])
+    where 
+        fourth (a,b,c,d) = d
+        step x (prefixsum,maxsum,maxprefix,maxsegment)
+            | x + prefixsum > maxsum = (x + prefixsum, x + prefixsum, x : maxprefix, x : maxprefix)
+            | x + prefixsum >= 0 = (x + prefixsum, maxsum, x : maxprefix, maxsegment)
+            | otherwise = (0, maxsum, [], maxsegment)
--- assg8/Assg8Test.hs
+++ assg8/Assg8Test.hs
@@ -4,7 +4,8 @@ import Test.HUnit
 import Assg8
 
 main :: IO Counts
-main = runTestTT $ TestList [testSimpleMSS1, testSimpleMSS2]
+main = runTestTT $ TestList [testSimpleMSS1, testSimpleMSS2, testSimpleMSS3,
+                             testSmartMSS1, testSmartMSS2, testSmartMSS3]
 
 testSimpleMSS1 = TestCase $ assertEqual "simpleMSS"
                 [2,1]
@@ -13,3 +14,19 @@ testSimpleMSS1 = TestCase $ assertEqual "simpleMSS"
 testSimpleMSS2 = TestCase $ assertEqual "simpleMSS"
                 [7,1,-2,-1,-4,10]
                 $ simpleMSS [-4,-3,-7,7,1,-2,-1,-4,10]
+
+testSimpleMSS3 = TestCase $ assertEqual "simpleMSS"
+                [9,-1,4]
+                $ simpleMSS [9,-1,4,-5,-7,7,1,-2,-1,-4,10]
+
+testSmartMSS1 = TestCase $ assertEqual "smartMSS"
+                [2,1]
+                $ smartMSS [-4,-3,-7,2,1,-2,-1,-4]
+
+testSmartMSS2 = TestCase $ assertEqual "smartMSS"
+                [7,1,-2,-1,-4,10]
+                $ smartMSS [-4,-3,-7,7,1,-2,-1,-4,10]
+
+testSmartMSS3 = TestCase $ assertEqual "smartMSS"
+                [9,-1,4]
+                $ smartMSS [9,-1,4,-5,-7,7,1,-2,-1,-4,10]
