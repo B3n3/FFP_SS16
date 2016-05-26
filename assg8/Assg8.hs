f1aea19d048b5f7e0e5b16be77ba67f310792556
Valentin - Mihai Neacsu <neacsustoi_92@yahoo.com>
2016-05-26 20:23:32 +0200

    outCamp

--- assg8/Assg8.hs
+++ assg8/Assg8.hs
@@ -1,5 +1,6 @@
 module Assg8 where
 
+import Data.Array
 
 simpleMSS :: [Int] -> [Int]
 simpleMSS = snd . maximum . zipSum . segments
@@ -19,3 +20,21 @@ smartMSS = fourth . foldr step (0,0,[],[])
             | x + prefixsum > maxsum = (x + prefixsum, x + prefixsum, x : maxprefix, x : maxprefix)
             | x + prefixsum >= 0 = (x + prefixsum, maxsum, x : maxprefix, maxsegment)
             | otherwise = (0, maxsum, [], maxsegment)
+
+
+data Content = Tree | Tent | Empty deriving (Eq,Ord,Show)
+type Camp = Array (Int,Int) Content
+type Row = Int -- ausschliesslich Werte von 1 bis 8
+type Column = Int -- ausschliesslich Werte von 1 bis 8
+type LocationsOfTrees = [(Row,Column)]
+type TentsPerRow = [Int] -- Liste der Laenge 8, ausschliesslich Werte von 0 bis 4; Wert des i-ten Elements bezeichnet Zahl der Zelte in Reihe i
+type TentsPerColumn = [Int] -- Liste der Laenge 8, ausschliesslich Werte von 0 bis 4; Wert des j-ten Elements bezeichnet Zahl der Zelte in Spalte j
+
+outCamp :: Camp -> [[Char]]
+outCamp = group8 . map tochr . elems
+    where 
+        tochr Tree = 'B'
+        tochr Tent = 'Z'
+        tochr Empty = 'u'
+        group8 [] = []
+        group8 lst = (take 8 lst) : (group8 $ drop 8 lst)
--- assg8/Assg8Test.hs
+++ assg8/Assg8Test.hs
@@ -1,11 +1,13 @@
 module Assg8Test where
 
 import Test.HUnit
+import Data.Array
 import Assg8
 
 main :: IO Counts
 main = runTestTT $ TestList [testSimpleMSS1, testSimpleMSS2, testSimpleMSS3,
-                             testSmartMSS1, testSmartMSS2, testSmartMSS3]
+                             testSmartMSS1, testSmartMSS2, testSmartMSS3,
+                             testOutCamp]
 
 testSimpleMSS1 = TestCase $ assertEqual "simpleMSS"
                 [2,1]
@@ -30,3 +32,14 @@ testSmartMSS2 = TestCase $ assertEqual "smartMSS"
 testSmartMSS3 = TestCase $ assertEqual "smartMSS"
                 [9,-1,4]
                 $ smartMSS [9,-1,4,-5,-7,7,1,-2,-1,-4,10]
+
+testOutCamp = TestCase $ assertEqual "outCamp"
+                ["BZBZuuZB", "uuuuuuuu", "BZBZuuZB", "uuuuuuuu", "BZBZuuZB", "uuuuuuuu", "uuuuuuuu", "BZBZuuZB"]
+                $ outCamp $ listArray ((1,1),(8,8)) [Tree,  Tent,  Tree,  Tent,  Empty, Empty, Tent,  Tree,
+                                                     Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+                                                     Tree,  Tent,  Tree,  Tent,  Empty, Empty, Tent,  Tree,
+                                                     Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+                                                     Tree,  Tent,  Tree,  Tent,  Empty, Empty, Tent,  Tree,
+                                                     Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+                                                     Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,
+                                                     Tree,  Tent,  Tree,  Tent,  Empty, Empty, Tent,  Tree]
